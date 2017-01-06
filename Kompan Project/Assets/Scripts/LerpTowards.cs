d7eda76cc619daa9c49a8ff949139a18e0c4e687
Terralux <kenneth@berle.eu>
2017-01-06 16:01:15 +0100

    Added a more functionality to the scene

---
+++ Kompan Project/Assets/Scripts/LerpTowards.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpTowards : MonoBehaviour {
+
+	[HideInInspector]
+	public Transform target;
+
+	private Vector3 origin;
+
+	private float fraction;
+
+	[Range(0f,0.1f)]
+	public float speed;
+
+	// Use this for initialization
+	void Awake () {
+		origin = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null) {
+			fraction += 0.01f;
+			transform.position = Vector3.Lerp (origin, target.position, fraction);
+		}
+	}
+}
---
+++ Kompan Project/Assets/Scripts/SimpleTransform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTransform : MonoBehaviour {
+
+	public float speed = 25;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, -speed, 0));
+	}
+}
